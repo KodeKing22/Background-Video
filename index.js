4d956947383dd223d305e23da45a60dbe2ecb224
KodeKing22 <jonkylemars@yahoo.com>
2023-01-14 21:04:45 -0600

    Created const's in js file and addEventListner to give functionality to button.

---
+++ index.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".btn");
+const video = document.querySelector(".background-video");
+const fa = document.querySelector(".fa");
+
+btn.addEventListener("click", ()=>{
+    if(btn.classList.contains("pause")){
+        btn.classList.remove("pause");
+        video.play();
+        fa.classList.add("fa-play");
+        fa.classList.remove("fa-pause");
+    }else{
+        btn.classList.add("pause");
+        video.pause();
+        fa.classList.remove("fa-play");
+        fa.classList.add("fa-pause");
+
+    }
+})
